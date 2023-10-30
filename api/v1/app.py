63cb6db7f6900fb5fdc522fdca1b65fd5c7adb14
Ogollah <kogollah2011@gmail.com>
2023-10-30 23:48:36 +0300

    Not found error

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,7 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(error):
     """
@@ -18,12 +19,14 @@ def teardown(error):
     """
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """
     Error 404
     """
     return jsonify({'error': 'Not found'}), 404
+
 
 if __name__ == '__main__':
     app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
