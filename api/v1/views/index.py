b8c419b709a4dd366cb0e66174fa70096a114642
Ogollah <kogollah2011@gmail.com>
2023-10-30 23:34:26 +0300

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,12 @@ Define routes blueprints
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -14,3 +20,9 @@ def status():
     Return application status.
     """
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    classes = [Amenity, City, Place, Review, State, User]
+    json_dict = {cls.__name__.lower() + 's': storage.count(cls) for cls in classes}
+    return jsonify(json_dict)
