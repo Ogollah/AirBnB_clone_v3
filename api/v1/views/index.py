168953ebfadc9ebbacedbf1b36899e197cc8fc7b
Ogollah <kogollah2011@gmail.com>
2023-10-30 23:18:00 +0300

    Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Define route blueprints
+"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """
+    Return application status.
+    """
+    return jsonify({'status': 'OK'})
